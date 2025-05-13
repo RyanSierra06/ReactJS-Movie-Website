b1c355ce3b71a6442d5f2364f2e2a66590f05581
RyanSierra06 <rsierra@purdue.edu>
2025-05-12 23:01:51 -0500

    Finished Guide

---
+++ frontend/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "efba55288accab4f9441350d187eda05";
+const BASE_URL = "http://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(
+        `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+            query
+        )}`
+    );
+    const data = await response.json()
+    return data.results
+};
